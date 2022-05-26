5cf468ce9bfb36489aebfdf2cee104109fd324e2
Rubial Alom <rubelalam75@gmail.com>
2022-05-26 19:07:26 +0530

    manifest edit

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import Hero from "./Hero";
 import Announcement from "./Announcement";
 import Curriculum from "./Curriculum";
 import PerformanceShowcase from "./PerformanceShowcase";
-import FreeTutorial from "./FreeTutorial";
+// import FreeTutorial from "./FreeTutorial";
 import Contact from "./Contact";
 
 function Home() {
